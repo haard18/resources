9c5cefc854854070d1e9590ef88b90676b42af1d
haard18 <haardsolanki.itm@gmail.com>
2026-05-13 01:24:44 +0530

    Add neubrutalist paper-styled favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Resource Share',
   description: 'Team resource sharing board',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
