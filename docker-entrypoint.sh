ae2f67650ee27eea0ee56af8e22d6bba59599eb0
haard18 <haardsolanki.itm@gmail.com>
2026-05-13 01:10:39 +0530

    Add db push to entrypoint to create tables on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
+echo "Running database migration..."
+node ./node_modules/prisma/build/index.js db push --skip-generate
 echo "Starting Next.js..."
 exec node ./node_modules/next/dist/bin/next start
